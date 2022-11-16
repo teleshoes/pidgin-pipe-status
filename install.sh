bb419b04237b1ef030f670e5f916341c1e896cc8
Elliot Wolk <elliot.wolk@gmail.com>
2022-11-16 16:49:34 -0500

    install: do not overwrite config if it exists

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 set -x
 cp pidgin-pipe-status.pl ~/.purple/plugins/
-cp pidgin-pipe-status-config.properties ~/.purple/plugins/
+if [ ! -e ~/.purple/plugins/pidgin-pipe-status-config.properties ]; then
+  cp pidgin-pipe-status-config.properties ~/.purple/plugins/
+fi
